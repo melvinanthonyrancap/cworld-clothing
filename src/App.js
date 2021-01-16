f4b8bbfd1690e7f050b0f8166ee4a5901bba5f04
melvinanthonyrancap <mel.rancap@gmail.com>
2021-01-15 22:35:37 -0800

    Added Header component and links

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import Shop from './pages/shop/Shop.jsx';
-
-const Hats = () => {
-  return (
-      <div>
-          <h1>Hats Page</h1>
-      </div>
-  )
-}
+import Header from './components/header/header.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={Shop} />
-        </Switch>
-        
+        <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={Shop} />
+          </Switch>
       </div>
     );
   }
