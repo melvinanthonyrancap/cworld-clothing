e1bce1b495f1cfaa0a66d1acfd8804af997ba9d3
melvinanthonyrancap <mel.rancap@gmail.com>
2021-02-04 19:29:40 -0800

    updated firebase config setup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-
-const config = {
-    apiKey: "AIzaSyBNkbz9_X4CegRYMY5WUnc9ReNoDJzPZCk",
-    authDomain: "cworld-db.firebaseapp.com",
-    projectId: "cworld-db",
-    storageBucket: "cworld-db.appspot.com",
-    messagingSenderId: "373008325458",
-    appId: "1:373008325458:web:ddcbd14a3c03c75074788b",
-    measurementId: "G-RMWM3Q2Y15"
-}
+import config from './config'
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
--- test.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
-const firestore = firebase.firestore()
-
-firestore.collection('users').doc('5uBUPRVNIiGIAMlnoKCi').collection('cartItems').doc('LBdZucA6AVHoge6OYJoj')
